1b628c2eac042036936ae9306abc10c73d86a957
Elen <petrielen@outlook.com>
2021-11-30 13:50:58 -0300

    Feito a renderização das páginas e criado lógica para aplicar o activate do menu de acordo com a página selecionada. Incluído função que calcula e atualiza a idade da autora na página sobre.

--- src/app.py
+++ src/app.py
@@ -5,11 +5,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', pagina_selecionada = 'inicio')
 
 @app.route('/sobre')
 def sobre():
-    return render_template('sobre.html') 
+    idade = calcularIdade(date(1994,1,24))
+    return render_template('sobre.html', idade = idade, pagina_selecionada = 'sobre')
+
+@app.route('/projetos')
+def projetos():
+    return render_template('projetos.html', pagina_selecionada = 'projetos')    
+
+def calcularIdade(dataAniversario):
+    hoje = date.today()
+    idade = hoje.year - dataAniversario.year - ((hoje.month, hoje.day) < (dataAniversario.month, dataAniversario.day))
+    return idade
+
+    
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
